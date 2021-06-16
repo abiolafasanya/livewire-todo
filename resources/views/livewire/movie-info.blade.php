cee1f454ed9d2bcf39bd6b02689a6c46a4269f4a
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-06-16 22:45:16 +0100

    update cast

--- resources/views/livewire/movie-info.blade.php
+++ resources/views/livewire/movie-info.blade.php
@@ -46,6 +46,146 @@
     <div class="cast border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 text-gray-100">
             <h2 class="font-semibold text-4xl">Cast</h2>
+            <div class="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+   
+                <div class="mt-8 p-3">
+                    <a href="#" wire:click="info()">
+                        <img src="img/1.jpg" alt="image 1" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/2.jpg" alt="image 2" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/3.jpg" alt="image 3" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/4.jpg" alt="image 4" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/5.jpg" alt="image 5" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/6.jpg" alt="image 6" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/7.jpg" alt="image 7" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+                <div class="mt-8 p-3">
+                    <a href="#">
+                        <img src="img/8.jpg" alt="image 8" 
+                        class="hover:opacity-75 transition ease-in-out bg-blend-normal rounded">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="hover:text-gray-300">
+                            Movie Title
+                        </a>
+                        <div class="flex items-center text-gray-300 text-sm">
+                            <span class="material-icons text-yellow-400">star</span>
+                            <span class="">85%</span>
+                            <span class="mx-2">|</span>
+                            <span class="">June 15, 2021</span>
+                        </div>
+                    </div>
+               </div>
+            
+            </div>
         </div>
     </div>
     
